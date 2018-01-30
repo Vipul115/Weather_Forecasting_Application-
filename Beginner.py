093ced62755731eaa511d7bc02e9b350a755c989
Vipul Rustagi <vipulrustagi11@gmail.com>
2018-01-31 01:10:48 +0530

    Added starting page for input.

--- Beginner.py
+++ Beginner.py
@@ -9,8 +9,9 @@ import requests
 import pandas as pd
 import numpy as np
 import sys
-from PyQt5 import QtWidgets, QtGui
-from PyQt5 import QtCore
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import *
+from PyQt5.QtGui import  *
 
 # len(df['period'])
 # type(df['period'])
@@ -25,34 +26,38 @@ from PyQt5 import QtCore
 class Windows(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
-        df = self.scrapper()
+        self.setGeometry(50,50, 500,500)
+        lat = QtWidgets.QLineEdit(self)
+        long = QtWidgets.QLineEdit(self)
+        lat.setValidator(QDoubleValidator(60.0, 160.0, 3))
+        long.setValidator(QDoubleValidator(20.0, 80.0, 3))
+        print(long.text())
+        okbut = QtWidgets.QPushButton("OK", self)
+        latLabel = QtWidgets.QLabel("Enter Latitude", self)
+        longLabel = QtWidgets.QLabel("Enter Longitude", self)
+        okbut.move(150,100)
+        latLabel.move(20,50)
+        longLabel.move(220,50)
+        lat.move(100, 50)
+        long.move(300, 50)
+
+        self.show()
+        okbut.clicked.connect()
+
+        df = self.scrapper(lat.text(), long.text())
         self.mainfunc(df)
 
 
 
-    def scrapper(self):
 
+    def scrapper(self, lat, long):
+        page = requests.get("http://forecast.weather.gov/MapClick.php?lon="+str(-98)+"&lat="+str(47))
 
-
-
-        page = requests.get("http://forecast.weather.gov/MapClick.php?lat=37.7772&lon=-122.4168")
-
-
+        # http: // forecast.weather.gov / MapClick.php?lat = "+str(lat)+" & lon = "+str(long)
 
         soup = BeautifulSoup(page.content, 'html.parser')
         seven_day = soup.find(id='seven-day-forecast')
-        #tombstone = seven_day.find_all(class_='tombstone-container')
 
-
-
-        #overnight = tombstone[1].find('p', {'class': 'period-name'}).get_text()
-        #shortdesc = tombstone[1].find(class_='short-desc').get_text()
-        #temp = tombstone[1].find(class_='temp').get_text()
-
-        #img = tombstone[1].find("img")['title']
-
-        # To distinguish oen tab from other if it ahs multiple attributes like class/id/etc
-        # seven_day.find_all('p',{'class':'period-name','extra':'attributes'})
         seven_day.find_all('p', {'class': 'period-name'})
 
         periods = seven_day.select(".period-name")
@@ -69,7 +74,7 @@ class Windows(QtWidgets.QMainWindow):
     def mainfunc(self, df):
 
         self.setWindowTitle('Weather Forecast')
-        self.setWindowIcon(QtGui.QIcon('icon.png'))
+        self.setWindowIcon(QIcon('icon.png'))
         self.bg = QtWidgets.QLabel(self)
         self.bg.resize(2500, 1000)
         self.bg.setStyleSheet("background-image: url(bg12.png);")
@@ -85,7 +90,7 @@ class Windows(QtWidgets.QMainWindow):
         self.temp = []
         for i in range(0,8):
             self.temp.append(QtWidgets.QLabel(df['temperature'][i], self))
-            self.temp[i].setFont(QtGui.QFont('Times New Roman', 16))
+            self.temp[i].setFont(QFont('Times New Roman', 16))
             self.temp[i].move(100+(i*150), 300)
 
 
@@ -96,21 +101,21 @@ class Windows(QtWidgets.QMainWindow):
             self.sd[i].move(100+(i*150), 550)
             self.pic.append(QtWidgets.QLabel(self))
             if "Cloudy" in df['short_desc'][i]:
-                self.pic[i].setPixmap(QtGui.QPixmap('c3.svg').scaled(115,115, QtCore.Qt.KeepAspectRatio))
+                self.pic[i].setPixmap(QPixmap('c3.svg').scaled(115,115, Qt.KeepAspectRatio))
 
             else:
                 if "Rainy" in df['short_desc'][i]:
 
-                    self.pic[i].setPixmap(QtGui.QPixmap('rainy.png').scaled(115,115, QtCore.Qt.KeepAspectRatio))
+                    self.pic[i].setPixmap(QPixmap('rainy.png').scaled(115,115, Qt.KeepAspectRatio))
                 else:
                     if "Sunny" in df['short_desc'][i]:
-                        self.pic[i].setPixmap(QtGui.QPixmap('sunny.svg').scaled(115,115, QtCore.Qt.KeepAspectRatio))
+                        self.pic[i].setPixmap(QPixmap('sunny.svg').scaled(115,115, Qt.KeepAspectRatio))
 
             self.pic[i].move(60 + (i * 152), 400)
             self.pic[i].resize(170,170)
 
 
-        b1.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        b1.clicked.connect(QCoreApplication.instance().quit)
 
         self.showMaximized()
 
@@ -118,4 +123,5 @@ class Windows(QtWidgets.QMainWindow):
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     win_obj = Windows()
+    win_obj.show()
     sys.exit(app.exec_())
