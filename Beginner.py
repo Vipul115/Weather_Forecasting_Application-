db48215da15ecb4947d0b93bf12701d63d446f60
Vipul Rustagi <vipulrustagi11@gmail.com>
2018-02-01 01:01:11 +0530

    Added window for taking inputs for latitudes and longitudes

--- Beginner.py
+++ Beginner.py
@@ -23,35 +23,45 @@ from PyQt5.QtGui import  *
 # In[3]:
 
 
-class Windows(QtWidgets.QMainWindow):
+class Windows1(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setGeometry(50,50, 500,500)
-        lat = QtWidgets.QLineEdit(self)
-        long = QtWidgets.QLineEdit(self)
-        lat.setValidator(QDoubleValidator(60.0, 160.0, 3))
-        long.setValidator(QDoubleValidator(20.0, 80.0, 3))
-        print(long.text())
-        okbut = QtWidgets.QPushButton("OK", self)
+        self.lat = QtWidgets.QLineEdit(self)
+        self.long = QtWidgets.QLineEdit(self)
+        self.okbut = QtWidgets.QPushButton("OK", self)
         latLabel = QtWidgets.QLabel("Enter Latitude", self)
         longLabel = QtWidgets.QLabel("Enter Longitude", self)
-        okbut.move(150,100)
-        latLabel.move(20,50)
-        longLabel.move(220,50)
-        lat.move(100, 50)
-        long.move(300, 50)
-
+        self.okbut.move(150, 100)
+        latLabel.move(20, 50)
+        longLabel.move(220, 50)
+        self.lat.move(100, 50)
+        self.long.move(300, 50)
+        self.okbut.clicked.connect(self.on_button_click)
         self.show()
-        okbut.clicked.connect()
 
-        df = self.scrapper(lat.text(), long.text())
+    def on_button_click(self):
+        x = self.lat.text()
+        y = self.long.text()
+        self.dailog = Windows2(x, y)
+        self.dailog.show()
+        self.close()
+
+
+
+class Windows2(QtWidgets.QMainWindow):
+    def __init__(self, lat1, long1):
+        super().__init__()
+        self.setGeometry(50,50, 500,500)
+
+        df = self.scrapper(lat1, long1)
+        """lat.text(), long.text()"""
         self.mainfunc(df)
 
 
 
 
-    def scrapper(self, lat, long):
-        page = requests.get("http://forecast.weather.gov/MapClick.php?lon="+str(-98)+"&lat="+str(47))
+    def scrapper(self, lat2, long2):
+        page = requests.get("http://forecast.weather.gov/MapClick.php?lon="+str(long2)+"&lat="+str(lat2))
 
         # http: // forecast.weather.gov / MapClick.php?lat = "+str(lat)+" & lon = "+str(long)
 
@@ -59,7 +69,7 @@ class Windows(QtWidgets.QMainWindow):
         seven_day = soup.find(id='seven-day-forecast')
 
         seven_day.find_all('p', {'class': 'period-name'})
-
+        # place = soup.find()
         periods = seven_day.select(".period-name")
         periods = [days.get_text() for days in periods]
         short_desc = [sd.get_text() for sd in seven_day.select('.short-desc')]
@@ -82,23 +92,25 @@ class Windows(QtWidgets.QMainWindow):
         b1 = QtWidgets.QPushButton('Quit', self)
         b1.move(620, 640)
         self.pd =[]
-        for i in range(0,8):
+        for i in range(0,9):
             self.pd.append(QtWidgets.QLabel(df['period'][i], self))
-            self.pd[i].move(100 + (i * 150), 200)
+            self.pd[i].move(60 + (i * 150), 200)
 
 
         self.temp = []
-        for i in range(0,8):
+        for i in range(0,9):
             self.temp.append(QtWidgets.QLabel(df['temperature'][i], self))
             self.temp[i].setFont(QFont('Times New Roman', 16))
-            self.temp[i].move(100+(i*150), 300)
+            self.temp[i].move(60+(i*150), 270)
 
 
         self.sd = []
         self.pic = []
-        for i in range(0,8):
+        for i in range(0,9):
             self.sd.append(QtWidgets.QLabel(df['short_desc'][i], self))
-            self.sd[i].move(100+(i*150), 550)
+            self.sd[i].move(60+(i*150), 400)
+            self.sd[i].setWordWrap(True)
+            self.sd[i].setGeometry(QRect(60+(i*150), 400,100,100))
             self.pic.append(QtWidgets.QLabel(self))
             if "Cloudy" in df['short_desc'][i]:
                 self.pic[i].setPixmap(QPixmap('c3.svg').scaled(115,115, Qt.KeepAspectRatio))
@@ -109,10 +121,10 @@ class Windows(QtWidgets.QMainWindow):
                     self.pic[i].setPixmap(QPixmap('rainy.png').scaled(115,115, Qt.KeepAspectRatio))
                 else:
                     if "Sunny" in df['short_desc'][i]:
-                        self.pic[i].setPixmap(QPixmap('sunny.svg').scaled(115,115, Qt.KeepAspectRatio))
+                        self.pic[i].setPixmap(QPixmap('sunny.svg').scaled(80,80, Qt.KeepAspectRatio))
 
-            self.pic[i].move(60 + (i * 152), 400)
-            self.pic[i].resize(170,170)
+            self.pic[i].move(40+(i*150), 320)
+            self.pic[i].resize(100,100)
 
 
         b1.clicked.connect(QCoreApplication.instance().quit)
@@ -122,6 +134,6 @@ class Windows(QtWidgets.QMainWindow):
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    win_obj = Windows()
+    win_obj = Windows1()
     win_obj.show()
     sys.exit(app.exec_())
